45b17120d7031e0af877c9dd98f0399a44db021b
NayaRPL <naya.mmj@gmail.com>
2022-02-26 21:59:00 +0800

    day 77

---
+++ uji kembali pemahaman.py
@@ -0,0 +1,13 @@
+# soal : roni memiliki penghasilan sebesar RP.200.000 per bulan. 
+# penghasilan tidak kena pajak adalah Rp 1.000.000 .jika pajak penghasilan
+# roni adalah 12% maka besar penghasilan roni adalah .....
+penghasilan_sebesar = 2000000
+penghasilan_tidak_kena_pajak= 1000000
+pajak_penghasilan=12/100
+penghasilan_kotor=1000000*12/100
+penghasilan=1000000-penghasilan_kotor
+penghasilan_bersih=penghasilan+penghasilan_tidak_kena_pajak
+print("penghasilan_kotor:",penghasilan_kotor)
+print("penghasilan:",penghasilan)
+print("penghasilan_bersih:",penghasilan_bersih)
+ 
